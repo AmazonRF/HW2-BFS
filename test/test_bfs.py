de5bd83624ca0809fb4838a556edf960b7adf59b
AmazonRF <amazonrf@163.com>
2024-01-24 11:54:08 -0800

    add test_bf connect and not connect

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -24,6 +24,8 @@ def test_bfs_traversal():
     # with pytest.raises(ValueError):pass
     assert my_bfs_path == groundTruth
 
+    #question, if multi path?
+
 def test_bfs():
     """
     TODO: Write your unit test for your breadth-first 
@@ -35,6 +37,36 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
+    tiny_graph_nx = nx.read_adjlist("/Users/yifeichen/Desktop/winter24/HW2-BFS/data/citation_network.adjlist", create_using=nx.DiGraph, delimiter=";")
+    tiny_graph_my = graph.Graph("/Users/yifeichen/Desktop/winter24/HW2-BFS/data/citation_network.adjlist")
+    
+    ##### connected ########
+    
+    start_node = 'Hani Goodarzi'
+    end_node  = 'Lani Wu'
+
+    my_bfs_path = tiny_graph_my.bfs(start_node,end_node)
+
+    are_connected = nx.has_path(G, start_node, end_node)
+    if are_connected:
+        nx_path = nx.shortest_path_length(tiny_graph_nx, source=start_node, target=end_node)
+        assert len(my_bfs_path) == nx_path
+    else:
+        nx_path = None
+        assert my_bfs_path == nx_path
 
     
-    pass
+
+    ##### not connected ########
+    start_node = '34356065'
+    end_node  = '34970257'
+
+    my_bfs_path = tiny_graph_my.bfs(start_node,end_node)
+
+    are_connected = nx.has_path(G, start_node, end_node)
+    if are_connected:
+        nx_path = nx.shortest_path_length(tiny_graph_nx, source=start_node, target=end_node)
+        assert len(my_bfs_path) == nx_path
+    else:
+        nx_path = None
+        assert my_bfs_path == nx_path
