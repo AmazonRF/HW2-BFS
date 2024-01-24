d731c42fdee79c15a875f1e2489c233360f0be12
AmazonRF <amazonrf@163.com>
2024-01-24 12:06:34 -0800

    add more case on unitest

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -14,15 +14,25 @@ def test_bfs_traversal():
     tiny_graph_nx = nx.read_adjlist("./data/tiny_network.adjlist", create_using=nx.DiGraph, delimiter=";")
     tiny_graph_my = graph.Graph("./data/tiny_network.adjlist")
     
+    ##### test breadth-first traversal ########
     start_node = list(tiny_graph_nx.nodes())[0]
     
     groundTruth = list(nx.bfs_edges(tiny_graph_nx, start_node))
     groundTruth = [start_node] + [v for u, v in groundTruth]
 
     my_bfs_path = tiny_graph_my.bfs(start_node)
-
-    # with pytest.raises(ValueError):pass
     assert my_bfs_path == groundTruth
+
+    ##### test not exist node ########
+    start_node = 'Yifei Chen"
+    my_bfs_path = tiny_graph_my.bfs(start_node)
+    assert my_bfs_path == None
+
+    ##### test empty graph ########
+    tiny_graph_my = graph.Graph("./data/emptyGraph.adjlist")
+
+    with pytest.raises(AttributeError):
+        tiny_graph_my.node
 
     #question, if multi path?
 
@@ -70,3 +80,5 @@ def test_bfs():
     else:
         nx_path = None
         assert my_bfs_path == nx_path
+
+
